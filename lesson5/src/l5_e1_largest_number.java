a669428bc41b897ca85bd4f55ce994c1be81daef
mimmato <mimmato@gmail.com>
2025-01-08 16:27:43 +0200

    uploading e1 of lesson 5, using for-each only

---
+++ lesson5/src/l5_e1_largest_number.java
@@ -0,0 +1,28 @@
+//import java.util.Scanner;
+
+
+public class l5_e1_largest_number {
+    public static void main(String[] args) {
+
+//        Scanner scanner = new Scanner(System.in);
+// for each loop
+
+        int[] numbers = {112, 15, 32, 42, 55, 75, 122, 132, 200, 150, 180};
+
+        int biggestNumber = numbers[0];
+
+        for(int number : numbers) {
+//            System.out.println("Current biggest number is: " + biggestNumber);
+//            System.out.println("Current number is " + number);
+            if (biggestNumber < number){
+                System.out.println("Comparing the biggest number: " + biggestNumber + " to the current number: " + number);
+//                break;
+                biggestNumber = number;
+            }
+        }
+        System.out.println("The biggest number from the array is: " + biggestNumber);
+
+
+
+        }
+    }
