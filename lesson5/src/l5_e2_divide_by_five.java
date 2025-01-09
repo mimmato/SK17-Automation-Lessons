8b41cc39484d73593355245c7c8ad13a7291a9ef
mimmato <mimmato@gmail.com>
2025-01-09 16:30:19 +0200

    completed exercise 2 of lesson 5, all loops

--- lesson5/src/l5_e2_divide_by_five.java
+++ lesson5/src/l5_e2_divide_by_five.java
@@ -5,26 +5,54 @@ public class l5_e2_divide_by_five {
         // exit loop when a number greater than 150 is checked
 
 
-        int[] i = {12, 15, 32, 42, 55, 75, 122, 132, 150, 180, 200};
+        int[] numbers = {12, 15, 32, 42, 55, 75, 122, 132, 150, 180, 200};
+
+        // working for each loop
+//        for (int number : numbers) {
+//            if (number <= 150 && number % 5 == 0) {
+//                System.out.println("The current number divisible by 5 is: " + number);
+//
+//            }
 
 
-        for (int number : i) {
-            if(number <= 150 && number % 5 == 0){
-                System.out.println("The current number divisible by 5 is: " + number);
+        // working for loop
+//        for (int i = 0; i < numbers.length; i++) {
+//            if (numbers[i] % 5 == 0 && numbers[i] <= 150) {
+//                System.out.println("The following numbers from the list are divisible by 5 and are NOT greater than 150: " + numbers[i]);
+//
+//            }
+
+
+//        int num = 145;
+//        while (num <= 150) {
+//            num++;
+//            System.out.println(num);
+//            num++;
+//        }
+
+
+
+        // working while loop
+//        int i = 0;
+//
+//        while (i < numbers.length) {
+//            if (numbers[i] % 5 == 0 && numbers[i] <= 150) {
+//                System.out.println("The following numbers from the list are divisible by 5 and are NOT greater than 150: " + numbers[i]);
+//            }
+//            i++;
+//        }
+
+
+        // working do while loop
+        int i = 0;
+        do {
+            if (numbers[i] % 5 == 0 && numbers[i] <= 150) {
+                System.out.println("The following numbers from the list are divisible by 5 and are NOT greater than 150: " + numbers[i]);
+            }            i++;
+        }
+        while(i < numbers.length);
 
             }
-
-
-
-//        for(int i=0; i < i.length; i++) {
-//            System.out.println(numbers[i]);
-
-
-//        while(int i < 150) {
-//            System.out.println("This number is divisible by 5: " + i);
-//            i++;
         }
-    }
-}
 
 
