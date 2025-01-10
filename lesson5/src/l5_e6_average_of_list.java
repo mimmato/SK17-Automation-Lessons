ac1fa61fff4aa54215da94a9822a715bcb2f42a8
mimmato <mimmato@gmail.com>
2025-01-10 21:06:39 +0200

    leaving e7 of l5 for later

---
+++ lesson5/src/l5_e6_average_of_list.java
@@ -0,0 +1,36 @@
+public class l5_e6_average_of_list {
+    public static void main(String[] args) {
+
+        int[] arrayList = {2, 5, 22, 10, 4};
+
+        // for loop
+//        double sum = 0;
+//        int i = 0;
+//        for (i=0; i < arrayList.length; i++){
+//            sum += arrayList[i];
+
+        // to check sum of the assignment operator as the loop goes though all numbers
+//            System.out.println(sum);
+//        }
+//        System.out.println("Average = sum divided by array length: " + sum / arrayList.length);
+
+
+        // for each
+//        double sum = 0;
+//        for(int i : arrayList){
+//            sum += i;
+//
+//            // to check sum of the assignment operator as the loop goes though all numbers
+//            System.out.println(sum);
+//        }
+//        System.out.println("Average = sum divided by array length: " + sum / arrayList.length);
+
+
+        // while
+
+
+
+
+
+    }
+}
---
+++ lesson5/src/l5_e7_numbers_guessing.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class l5_e7_numbers_guessing {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Give me a number between 1 and 100: ");
+        int randomNumber = scanner.nextInt();
+
+
+
+
+//        int i = 0;
+//
+//        for (int i = 0; i <= 100; i++){
+//
+//        }
+
+//        int[] arrayList = new int[101];
+//
+//        for(int i : arrayList){
+//            System.out.println("Current number is: " + arrayList.length);
+//        }
+    }
+}
