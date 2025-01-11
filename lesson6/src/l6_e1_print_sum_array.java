0a2aeaa52d48d350fbe31daed993be0d9c3dbdc1
mimmato <mimmato@gmail.com>
2025-01-11 15:41:24 +0200

    finished e1 of l6

---
+++ lesson6/src/l6_e1_print_sum_array.java
@@ -0,0 +1,26 @@
+public class l6_e1_print_sum_array {
+    public static void main(String[] args) {
+
+        int[] arrayList = {2, 5, 7, 10};
+
+//        System.out.println(arrayList.length);
+
+//        int sum = 0;
+//
+//        for (int i = 0; i < arrayList.length; i++){
+//            sum += arrayList[i];
+//
+//        }
+//        System.out.println("The sum generated with a For loop is: " + sum);
+
+
+            // for each loop
+
+//        for (int i : arrayList){
+//            sum +=  i;
+//        }
+//
+//        System.out.println("The sum generated with a For Each loop is: " sum);
+
+    }
+}
