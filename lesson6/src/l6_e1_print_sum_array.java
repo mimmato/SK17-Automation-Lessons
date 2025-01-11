786cfab0d2e05744d4b3ecc854d08667323488c3
mimmato <mimmato@gmail.com>
2025-01-11 15:39:08 +0200

    finished with lesson 5, commiting to branch lesson5

---
+++ lesson6/src/l6_e1_print_sum_array.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class l6_e1_print_sum_array {
+    public static void main(String[] args) {
+
+        int[] arrayList = {2, 5, 7, 10};
+
+//        System.out.println(arrayList.length);
+
+//        int sum = 0;
+//
+//        for (int i = 0; i < arrayList.length; i++){
+//            sum += arrayList[i];
+//
+//        }
+//        System.out.println("The sum generated with a For loop is: " + sum);
+
+
+
+//        for (int i : arrayList){
+//            sum +=  i;
+//        }
+//
+//        System.out.println("The sum generated with a For Each loop is: " sum);
+
+    }
+}
