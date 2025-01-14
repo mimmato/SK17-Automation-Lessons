f65be1824b27a5d8024f231c5702b79ffe9161e6
mimmato <mimmato@gmail.com>
2025-01-14 18:41:40 +0200

    adding a loop to cycle through the indexes, so the user can choose which item to manipulate

--- lesson6/src/l6_e5_toDo_ArrayList.java
+++ lesson6/src/l6_e5_toDo_ArrayList.java
@@ -14,13 +14,13 @@ public class l6_e5_toDo_ArrayList {
         System.out.println("Please add a new task: ");
         String addTask = scanner.nextLine();
         arrayList.add(addTask);
-
-        while(true){
+        // add to array
+        while (true) {
 
             System.out.println("Do you want to add more tasks? yes/no");
             String choice = scanner.nextLine();
 
-            if(choice.equals("no")){
+            if (choice.equals("no")) {
                 break;
             } else {
                 System.out.println("Please enter your new task: ");
@@ -30,12 +30,13 @@ public class l6_e5_toDo_ArrayList {
         }
 
         System.out.println("Here are the items in the ArrayList: " + arrayList);
-
-        while (true){
+        // set / modify array
+        // sub tasks to be able to modify/set an item based on index
+        while (true) {
             System.out.println("Do you wish to modify the previous task?");
             String modify = scanner.nextLine();
 
-            if(modify.equals("no")){
+            if (modify.equals("no")) {
                 break;
             } else {
                 System.out.println("Please type in the changes to the previous task or type in completed: ");
@@ -43,35 +44,50 @@ public class l6_e5_toDo_ArrayList {
                 arrayList.set(0, newTaskModified);
             }
         }
-
+        for (int i = 0; i < arrayList.size(); i++) {
+            System.out.println("Index is: " + i + " and value is " + arrayList.get(i));
+        }
         System.out.println("Here are the items in the ArrayList: " + arrayList);
 
-        while (true){
-            System.out.println("Do you wish to remove the previous task?");
-            String remove = scanner.nextLine();
+        // remove item from array
+        // sub tasks to be able to remove an item based on index
 
-            if(remove.equals("no")){
+        while (true) {
+            System.out.println("Do you wish to remove a previous task?");
+            String remove = scanner.nextLine();
+            if (remove.equals("no")) {
                 break;
             } else {
                 System.out.println("Please type in the index of the task you want to remove: ");
-                String taskToRemove = scanner.nextLine();
-                arrayList.remove(0);
+                int indexToRemove = scanner.nextInt();
+                scanner.nextLine(); // adding as the preceding nextInt will cause failure
+
+                if (indexToRemove >= 0 && indexToRemove < arrayList.size()){
+                    arrayList.remove(indexToRemove);
+                    System.out.println("Removed index: " + indexToRemove);
+                } else {
+                    System.out.println("No such index number");
+                }
             }
         }
-        while(true){
-            System.out.println("Do you wish to remove all completed tasks? yes/no");
-                String removeCompleted = scanner.nextLine();
-                    if(removeCompleted.equals("no")){
-                        break;
-                    } else {
-                        arrayList.removeIf(removedTasks -> removedTasks.toLowerCase().contains("completed"));
-                    }
-            System.out.println("Here are the items in the ArrayList: " + arrayList);
+
+
+        for (int i = 0; i < arrayList.size(); i++) {
+            System.out.println("Index is: " + i + " and value is " + arrayList.get(i));
         }
 
+        while (true) {
+            System.out.println("Do you wish to remove all completed tasks? yes/no");
+            String removeCompleted = scanner.nextLine();
+            if (removeCompleted.equals("no")) {
+                break;
+            } else {
+                arrayList.removeIf(removedTasks -> removedTasks.toLowerCase().contains("completed"));
+            }
+            System.out.println("Here are the items in the ArrayList: " + arrayList);
 
 
-        System.out.println("Here are the items in the ArrayList: " + arrayList);
+            System.out.println("Here are the items in the ArrayList: " + arrayList);
 
 
 //        System.out.println("Do you want to add more tasks?");
@@ -85,5 +101,7 @@ public class l6_e5_toDo_ArrayList {
 //
 //        System.out.println("This is the task you added: " + arrayList.get(0));
 
+        }
     }
 }
+
